25d5803b4284b8cc963a39aa6f533df682bbfcf3
Harsh Patil <harshpatil53342@gmail.com>
2026-03-10 09:32:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const cards = document.querySelectorAll(".card")
+
+window.addEventListener("scroll",()=>{
+
+cards.forEach(card=>{
+
+let top = card.getBoundingClientRect().top
+
+if(top < window.innerHeight - 100){
+
+card.style.opacity="1"
+card.style.transform="translateY(0)"
+
+}
+
+})
+
+})
